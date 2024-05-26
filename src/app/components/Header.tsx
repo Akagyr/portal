43c340a574d2ab429664b0ed9eec312763d4841f
Alex <casha.mironyuk.2001@gmail.com>
2024-05-26 17:05:07 +0300

    Removed adding questions page

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,20 +6,12 @@ export default function Header() {
       <Link href='/' className='uppercase text-white font-bold text-[26px] lg:text-[30px]'>
         Portal
       </Link>
-      <div className='flex gap-[10px] items-center'>
-        <Link
-          href='quiz'
-          className='text-white lg:hover:bg-emerald-600 lg:py-[10px] lg:px-[20px] rounded-full text-xs lg:text-sm duration-200 max-w-[80px] lg:max-w-full text-center lg:text-left'
-        >
-          Пройти тестування
-        </Link>
-        <Link
-          href='createNewQuestion'
-          className='text-white lg:hover:bg-emerald-600 lg:py-[10px] lg:px-[20px] rounded-full text-xs lg:text-sm duration-200 max-w-[80px] lg:max-w-full text-center lg:text-left'
-        >
-          Додати питання
-        </Link>
-      </div>
+      <Link
+        href='quiz'
+        className='text-white lg:hover:bg-emerald-600 lg:py-[10px] lg:px-[20px] rounded-full text-xs lg:text-sm duration-200 max-w-[80px] lg:max-w-full text-center lg:text-left'
+      >
+        Пройти тестування
+      </Link>
     </header>
   );
 }
