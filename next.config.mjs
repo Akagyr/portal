8e9c8a4532bc4a22740fcede7376d4a13d4926aa
Alex <casha.mironyuk.2001@gmail.com>
2024-05-30 18:01:07 +0300

    Fixed static questions data in quiz page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/quiz',
+            has: [
+              {
+                type: 'query',
+                key: 'no-cache',
+                value: '1',
+              },
+            ],
+            destination: '/quiz',
+          },
+        ];
+      },
+};
 
 export default nextConfig;
--- src/app/(pages)/quiz/page.tsx
+++ src/app/(pages)/quiz/page.tsx
@@ -1,8 +1,27 @@
+import { Question } from '@/app/lib/types';
+import { getDocs, collection } from 'firebase/firestore';
+import { db } from '@/app/lib/firebase';
 import Quiz from '@/app/components/Quiz';
-import { getRandomQuestions } from '@/app/lib/helpers';
 
 export default async function QuizPage() {
-  const questions = await getRandomQuestions();
+  const querySnapshot = await getDocs(collection(db, 'questions'));
+  const questions = querySnapshot.docs.map(
+    (doc) =>
+      ({
+        ...doc.data(),
+      } as Question)
+  );
 
-  return <Quiz questions={questions} />;
+  function shuffleArray(array: Question[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  const shuffledQuestions = shuffleArray(questions);
+  const selectedQuestions = shuffledQuestions.slice(0, 10);
+
+  return <Quiz questions={selectedQuestions} />;
 }
